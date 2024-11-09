bca991179841ef971068db78dccac21d7c436716
Nicholas Chin <bychinicky2005@gmail.com>
2024-11-09 00:10:44 -0700

    Initial Commit

---
+++ src/Transactions.rs
@@ -0,0 +1,13 @@
+
+
+/*pub struct Transaction{
+    pub sender: String,
+    pub recipient: String,
+    pub amount: usize,
+}
+
+impl Transaction{
+    pub fn new(sender: String, recipient: String, amount: usize) ->Transaction{
+
+    }
+}
